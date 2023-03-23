a4a9361c86416f5ee0b68564cf74bb797828c7fa
brokeninfp <brokeninfp@gmail.com>
2023-03-24 01:36:25 +0200

    adding scalar product feature

--- first_try_task.c
+++ first_try_task.c
@@ -15,7 +15,6 @@ int main (void)
 	printf("%d",task);
 	if(task == 1)
 	{
-		printf("%d",task);
 		float first_v , second_v ,thetanrad , thetandeg , result;
 
 		printf("enter the two vectors as scale of A & B and THETA \n");
@@ -28,7 +27,19 @@ int main (void)
 		thetanrad = thetandeg * M_PI / 180.0;
 		result = first_v * second_v * sin(thetanrad);
 		printf("the cross product is %f",result);
+	}else if (task == 2)
+	{
+		float first_v , second_v ,thetanrad , thetandeg , result;
 
-
+		printf("enter the two vectors as scale of A & B and THETA \n");
+		printf("\t first vector\t");
+		scanf("%f", &first_v);
+		printf("\t second vector\t");
+		scanf("%f", &second_v);
+		printf("\t seta\t");
+		scanf("%f", &thetandeg);
+		thetanrad = thetandeg * M_PI / 180.0;
+		result = first_v * second_v * cos(thetanrad);
+		printf("the scalar product is %f",result);
 	}
 }
