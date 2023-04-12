0c7228a58803761be6a41c1bbfd69d22aa331126
brokeninfp <brokeninfp@gmail.com>
2023-04-12 23:01:46 +0200

    adding 3 tasks 12/4/2023

--- 4_tasks/square.c
+++ 4_tasks/square.c
@@ -4,45 +4,44 @@
 
 /**
  * main - function to solve prabola equation
- * Return :0
+ * Return: 0
  */
-int main (void)
+int main(void)
 {
-	float a ,b ,c,spec,solve;
+	float a, b, c, spec, solve;
 
 	printf("write the equation as a x² + b x + c = 0 \n");
-	scanf(" %f %f %f",&a ,&b,&c);
-	printf("your equation is %.1fx² + %.1fx + %.1f  \n",a ,b,c);
-	spec=(pow(b,2))-(4*a*c);
+	scanf(" %f %f %f", &a, &b, &c);
+	printf("your equation is %.1fx² + %.1fx + %.1f  \n", a, b, c);
+	spec = (pow(b, 2)) - (4 * a * c);
 	if (a == 0)
 	{
 		printf("this equation isn't prabola \n");
 	}
-	else if(spec==0)
+	else if (spec == 0)
 	{
-		solve=-b/2*a;
-		printf("this equatino has one solution = %.1f \n",solve);
+		solve = -b / 2 * a;
+		printf("this equatino has one solution = %.1f \n", solve);
 	}
-	else if(spec >0)
+	else if (spec > 0)
 	{
-		float solution1 ,solution2;
+		float solution1, solution2;
 
-		solution1 = (-b+sqrt(spec))/(2*a);
-		solution2 = (-b-sqrt(spec))/(2*a);
+		solution1 = (-b + sqrt(spec)) / (2 * a);
+		solution2 = (-b - sqrt(spec)) / (2 * a);
 		printf("this equatino has two solutions \n");
-		printf("first solution = %f \n",solution1);
-		printf("second solution = %f \n",solution2);
+		printf("first solution = %f \n", solution1);
+		printf("second solution = %f \n", solution2);
 	}
-	else if(spec <0)
+	else if (spec < 0)
 	{
-		float  solution1 ,solution2,img,real;
+		float  solution1, solution2, img, real;
 
-		real=-b/2*a;
-		img=sqrt(-spec)/2*a;
+		real = -b / 2 * a;
+		img = sqrt(-spec) / 2 * a;
 		printf("this equatino has two imagin solutions \n");
-		printf("first solution = %.2f + %.2f i \n",real ,img);
-		printf("second solution = %.2f - %.2f i\n",real ,img);
+		printf("first solution = %.2f + %.2f i \n", real, img);
+		printf("second solution = %.2f - %.2f i\n", real, img);
 	}
-	
-	return 0;
+	return (0);
 }
