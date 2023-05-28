e4976ed6b4203d2c178e6daa19dbdf8f8f3da16a
brokeninfp <brokeninfp@gmail.com>
2023-05-29 01:20:19 +0300

    last update

--- Derivative_distance.py
+++ Derivative_distance.py
@@ -1,8 +1,6 @@
 from sympy import *
 t = symbols('t')
 deci = int(input("choce an operatin  \n1 - Deriving velocity from distance\n2 - Deriving acceleration from distance\n3- Deriving acceleration from velocity\n "))
-#func = input("enter the equation on t as a*t**2+b*t+c \n")
-#deci = int(input("do you want to print \n1 - the velocity equation \n2 - the velocity  \n3 - the acceleratio\n "))
 if deci == 1:
     func = input("enter the distance equation on t as a*t**2+b*t+c \n")
     df = diff(func,t)
